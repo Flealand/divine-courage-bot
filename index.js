fc4717af49b40b04f2fb241c9303f0c77215f430
Flealand <chrisgrass321@gmail.com>
2022-01-30 14:05:13 +0100

    creates json for items and boots, adds total build price to output

--- index.js
+++ index.js
@@ -5,98 +5,15 @@ const { token } = require("./config.json");
 const { MessageEmbed } = require("discord.js");
 
 const heroes = require("./heroes.json");
+const items = require("./items.json");
+const boots = require("./boots.json");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-const boots = ["Boots of Travel", "Phase Boots", "Mana Boots", "Treads", "Tranquil Boots"];
 
 const lanes = [
     "Safe lane",
     "Mid lane",
     "Off lane",
 ];
-
-const items = [
-    "Orb of Corrosion",
-    "Falcon Blade",
-    "Mask of Madness",
-    "Hand of Midas",
-    "Helm of the Dominator",
-    "Moon Shard",
-    "Helm of the Overlord",
-    "Urn of Shadows",
-    "Medallion of Courage",
-    "Drum of Endurance",
-    "Mekansm",
-    "Holy Locket",
-    "Vladmirs Offering",
-    "Spirit Vessel",
-    "Pipe of Insight",
-    "Guardian Greaves",
-    "Veil of Discord",
-    "Glimmer Cape",
-    "Force Staff",
-    "Aether Lens",
-    "Witch Blade",
-    "Solar Crest",
-    "Dagon",
-    "Euls Scepter of Divinity",
-    "Rod of Atos",
-    "Orchid Malevolence",
-    "Aghanims Scepter",
-    "Refresher Orb",
-    "Octarine Core",
-    "Scythe of Vyse",
-    "Aghanims Blessing",
-    "Gleipnir",
-    "Wind Waker",
-    "Hood of Defiance",
-    "Vanguard",
-    "Blade Mail",
-    "Aeon Disk",
-    "Eternal Shroud",
-    "Crimson Guard",
-    "Lotus Orb",
-    "Black King Bar",
-    "Hurricane Pike",
-    "Linkens Sphere",
-    "Manta Style",
-    "Shivas Guard",
-    "Heart of Tarrasque",
-    "Assault Cuirass",
-    "Bloodstone",
-    "Crystalys",
-    "Meteor Hammer",
-    "Armlet of Mordiggian",
-    "Skull Basher",
-    "Shadow Blade",
-    "Desolator",
-    "Battle Fury",
-    "Ethereal Blade",
-    "Nullifier",
-    "Butterfly",
-    "Monkey King Bar",
-    "Radiance",
-    "Daedalus",
-    "Divine Rapier",
-    "Bloodthorn",
-    "Abyssal Blade",
-    "Dragon Lance",
-    "Mage Slayer",
-    "Echo Sabre",
-    "Maelstrom",
-    "Diffusal Blade",
-    "Heavens Halberd",
-    "Kaya and Sange",
-    "Sange and Yasha",
-    "Yasha and Kaya",
-    "Satanic",
-    "Eye of Skadi",
-    "Mjollnir",
-    "Arcane Blink",
-    "Overwhelming Blink",
-    "Swift Blink",
-];
-
 
 client.on("ready", () => {
     console.log("Ready!");
@@ -108,28 +25,26 @@ client.on("interactionCreate", async interaction => {
     const { commandName } = interaction;
     if (commandName === "dc") {
 
-        const keys = Object.keys(heroes);
-        const randIndex = Math.floor(Math.random() * keys.length);
-        const randKey = keys[randIndex];
-        const hero = heroes[randKey];
+        const hero = getRandomFromJson(heroes);
+        const boot = getRandomFromJson(boots);
 
-        const boot = boots[Math.floor(Math.random() * boots.length)];
-
-        const item1 = items[Math.floor(Math.random() * items.length)];
-        const item2 = items[Math.floor(Math.random() * items.length)];
-        const item3 = items[Math.floor(Math.random() * items.length)];
-        const item4 = items[Math.floor(Math.random() * items.length)];
+        const item1 = getRandomFromJson(items);
+        const item2 = getRandomFromJson(items);
+        const item3 = getRandomFromJson(items);
+        const item4 = getRandomFromJson(items);
         const lane = lanes[Math.floor(Math.random() * lanes.length)];
 
-        results = [boot, hero, item1, item2, item3, item4];
-        console.log(interaction.user.tag + ": Hero pic url: " + hero.url + " Items", item1 + " | " + item2 + " | " + item3 + " | " + item4);
+        const totalBuildPrice = +item1.price + +item2.price + +item3.price + +item4.price  + +boot.price
+
+        console.log(interaction.user.tag + ": Hero pic url: " + hero.url + " Items", item1.display + " | " + item2.display + " | " + item3.display + " | " + item4.display);
 
         try {
             const embed = new MessageEmbed()
                 .setThumbnail("https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/" + hero.url + ".png")
                 .setTitle(hero.display + " - " + lane)
-                .setDescription(boot)
-                .addField("Items", item1 + " | " + item2 + " | " + item3 + " | " + item4, true);
+                .setDescription(boot.display)
+                .addField("Items", item1.display + " | " + item2.display + " | " + item3.display + " | " + item4.display, true)
+                .setFooter({ text: 'Total build price: ' + totalBuildPrice + " gold."});
             await interaction.reply({ embeds: [embed] });
 
 
@@ -146,5 +61,15 @@ client.on("interactionCreate", async interaction => {
         await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
     }
 });
+
+
+function getRandomFromJson(jsonObj) {
+
+    const keys = Object.keys(jsonObj);
+    const randIndex = Math.floor(Math.random() * keys.length);
+    const randKey = keys[randIndex];
+    return jsonObj[randKey];
+
+}
 
 client.login(token);
